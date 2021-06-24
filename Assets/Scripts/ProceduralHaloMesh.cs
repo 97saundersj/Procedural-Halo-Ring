805fafbb61b92e34b29a77febd5f2523fc62ce9e
97saundersj <jacques_s@hotmail.co.uk>
2021-06-24 16:44:02 +0100

    Started work on UV mapping

--- Assets/Scripts/ProceduralHaloMesh.cs
+++ Assets/Scripts/ProceduralHaloMesh.cs
@@ -3,16 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
-public class PolygonMesh : MonoBehaviour
+public class ProceduralHaloMesh : MonoBehaviour
 {
 	public int CircleSegmentCount;
 	public float width;
+	public bool renderBothSides;
 
 	private Mesh mesh;
 	private Vector3[] vertices;
 	private Vector3[] normals;
 	private Color32[] cubeUV;
 
+	private int CircleVertexCount;
+	private int CircleIndexCount;
 
 
 	private void Awake()
@@ -37,7 +40,7 @@ public class PolygonMesh : MonoBehaviour
 		WaitForSeconds wait = new WaitForSeconds(0.05f);
 
 		GetComponent<MeshFilter>().mesh = mesh = new Mesh();
-		mesh.name = "Procedural Sphere";
+		mesh.name = "Procedural Halo";
 		GenerateCircleMesh();
 
 		
@@ -45,85 +48,76 @@ public class PolygonMesh : MonoBehaviour
 
 	private Mesh GenerateCircleMesh()
 	{
-		int CircleVertexCount = (CircleSegmentCount * 2);
-		int CircleIndexCount = (CircleSegmentCount * 3 * 2 * 2);
-
 		var circle = mesh;
+
+		CircleVertexCount = (CircleSegmentCount * 2) + 2;
+		CircleIndexCount = renderBothSides ? (CircleSegmentCount * 3 * 2 * 2) : (CircleSegmentCount * 3 * 2);
+
 		var vert = new List<Vector3>(CircleVertexCount);
+		Vector2[] uv = new Vector2[CircleVertexCount];
 		var indices = new int[CircleIndexCount];
-		var segmentWidth = Mathf.PI * 2f / CircleSegmentCount;
-		var angle = 0f;
-		//vert.Add(Vector3.zero);
+		
 
 		//loop through each segment
-		for (int v = 0; v < CircleSegmentCount; v++)
+		var angle = 0f;
+		var segmentWidth = Mathf.PI * 2f / CircleSegmentCount;
+		for (int v = 0; v < CircleSegmentCount + 1; v++)
 		{
 			vert.Add(new Vector3(Mathf.Cos(angle), 0f, Mathf.Sin(angle)));
 			vert.Add(new Vector3(Mathf.Cos(angle), width, Mathf.Sin(angle)));
-
+			
 			angle -= segmentWidth;
-
+			
 			var startVert = v * 2;
-			var startIndex = v * 6 * 2;
+			var startIndex = v * 6 * (renderBothSides ? 2 : 1);
 
-			if ( v < CircleSegmentCount -1)
+			if ( v < CircleSegmentCount)
 			{
 				//First Triangle
 				indices[startIndex] = startVert;
 				indices[startIndex + 1] = startVert + 1;
 				indices[startIndex + 2] = startVert + 2;
 
-				//Second Triangle Triangle
+				//Second Triangle
 				indices[startIndex + 3] = startVert + 3;
 				indices[startIndex + 4] = startVert + 2;
 				indices[startIndex + 5] = startVert + 1;
 
+
+				//UVs
+				/*
+				uv[0] = new Vector2(0, 0);
+				uv[1] = new Vector2(0, 1);
+
+				uv[2] = new Vector2(0.5f, 0);
+				uv[3] = new Vector2(0.5f, 1);
+
+				uv[4] = new Vector2(1, 0);
+				uv[5] = new Vector2(1, 1);
+				*/
 				//Other Side for normals
-				//First Triangle
-				indices[startIndex + 6] = startVert + 2;
-				indices[startIndex + 7] = startVert + 1;
-				indices[startIndex + 8] = startVert ;
+				if (renderBothSides)
+                {
+					//First Triangle
+					indices[startIndex + 6] = startVert + 2;
+					indices[startIndex + 7] = startVert + 1;
+					indices[startIndex + 8] = startVert;
 
-				//Second Triangle Triangle
-				indices[startIndex + 9] = startVert + 1;
-				indices[startIndex + 10] = startVert + 2;
-				indices[startIndex + 11] = startVert + 3;
-
+					//Second Triangle Triangle
+					indices[startIndex + 9] = startVert + 1;
+					indices[startIndex + 10] = startVert + 2;
+					indices[startIndex + 11] = startVert + 3;
+				}
 			}
-			
-			else if(v == CircleSegmentCount - 1)
-            {
-				//First Triangle
-				indices[startIndex] = startVert;
-				indices[startIndex + 1] = startVert + 1;
-				indices[startIndex + 2] = 0;
-				
-				//Second Triangle Triangle
-				indices[startIndex + 3] = startVert + 1;
-				indices[startIndex + 4] = 1;
-				indices[startIndex + 5] = 0;
-
-				//Other Side for normals
-				//First Triangle
-				indices[startIndex + 6] = 0;
-				indices[startIndex + 7] = startVert + 1;
-				indices[startIndex + 8] = startVert;
-
-				//Second Triangle Triangle
-				indices[startIndex + 9] = 0;
-				indices[startIndex + 10] = 1;
-				indices[startIndex + 11] = startVert + 1;
-
-			}
-			
 		}
 
 		circle.SetVertices(vert);
+		circle.SetUVs(0, uv);
 		circle.SetIndices(indices, MeshTopology.Triangles, 0);
 		
 		circle.RecalculateBounds();
 
-		//circle.RecalculateNormals();
+		circle.RecalculateNormals();
 		//circle.RecalculateUVDistributionMetrics();
 
 
