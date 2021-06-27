878c4dde75b78b06b06689a8e50ad878aceab7cc
97saundersj <jacques_s@hotmail.co.uk>
2021-06-27 12:18:49 +0100

    Initial Code for adding an lod slider

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -3,42 +3,39 @@ using System.Collections;
 
 public static class MeshGenerator {
 
-	public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightScale, bool createHalo) {
+	public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier, AnimationCurve heightCurve, int levelOfDetail, bool createHalo) {
 		int width = heightMap.GetLength (0);
 		int height = heightMap.GetLength (1);
 		float topLeftX = (width - 1) / -2f;
 		float topLeftZ = (height - 1) / 2f;
 
+		int meshSimplificationIncrement = (levelOfDetail == 0) ? 1 : levelOfDetail * 2;
+		int verticesPerLine = (width - 1) / meshSimplificationIncrement + 1;
 
-
-		MeshData meshData = new MeshData (width, height);
-
+		MeshData meshData = new MeshData (verticesPerLine, verticesPerLine);
 		int vertexIndex = 0;
 
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
-
-				Vector3 vector;
+		for (int y = 0; y < height; y += meshSimplificationIncrement) {
+			for (int x = 0; x < width; x += meshSimplificationIncrement) {
 
 				if (createHalo)
-                {
+				{
 					float ang = x * Mathf.PI * 2f / width;
 					float currentWidth = y * 0.01f;
-					float heightMapNormalised = ((1 - heightMap[x, y]) * heightScale) + 1;
+					float heightMapNormalised = ((1 - heightMap[x, y]) * (heightCurve.Evaluate(heightMap[x, y]) * heightMultiplier)) + 1;
 
-					vector = new Vector3(Mathf.Cos(ang) * heightMapNormalised, currentWidth, Mathf.Sin(ang) * heightMapNormalised);
+					meshData.vertices[vertexIndex] = new Vector3(Mathf.Cos(ang) * heightMapNormalised, currentWidth, Mathf.Sin(ang) * heightMapNormalised);
 				}
 				else
                 {
-					vector = meshData.vertices[vertexIndex] = new Vector3(topLeftX + x, heightMap[x, y]* heightScale, topLeftZ - y);
-				}
+					meshData.vertices [vertexIndex] = new Vector3 (topLeftX + x, heightCurve.Evaluate(heightMap [x, y]) * heightMultiplier, topLeftZ - y);
+                }
 				
-				meshData.vertices[vertexIndex] = vector;
 				meshData.uvs [vertexIndex] = new Vector2 (x / (float)width, y / (float)height);
 
 				if (x < width - 1 && y < height - 1) {
-					meshData.AddTriangle (vertexIndex, vertexIndex + width + 1, vertexIndex + width);
-					meshData.AddTriangle (vertexIndex + width + 1, vertexIndex, vertexIndex + 1);
+					meshData.AddTriangle (vertexIndex, vertexIndex + verticesPerLine + 1, vertexIndex + verticesPerLine);
+					meshData.AddTriangle (vertexIndex + verticesPerLine + 1, vertexIndex, vertexIndex + 1);
 				}
 
 				vertexIndex++;
